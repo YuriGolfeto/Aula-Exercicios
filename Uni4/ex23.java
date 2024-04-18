d99de5195d1e81967ae7d732dd26459ed39d58ef
YuriSparda <yuri.sparda@hotmail.com>
2024-04-18 19:50:21 -0300

    Update 18/04/2024

--- Uni4/ex23.java
+++ Uni4/ex23.java
@@ -3,48 +3,89 @@ package Uni4;
 import java.util.Scanner;
 
 public class ex23 {
-    
+
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-    System.out.println("Digite o número do mês:");
-    int mes = sc.nextInt();
+        System.out.println("Digite o número do mês:");
+        int mes = sc.nextInt();
 
-    String mesExtenso = "";
+        String mesExtenso = "";
+        switch (mes) {
+            case 1:
+                mesExtenso = "Janeiro";
+                break;
+            case 2:
+                mesExtenso = "Fevereiro";
+                break;
+            case 3:
+                mesExtenso = "Março";
+                break;
+            case 4:
+                mesExtenso = "Abril";
+                break;
+            case 5:
+                mesExtenso = "Maio";
+                break;
+            case 6:
+                mesExtenso = "Junho";
+                break;
+            case 7:
+                mesExtenso = "Julho";
+                break;
+            case 8:
+                mesExtenso = "Agosto";
+                break;
+            case 9:
+                mesExtenso = "Setembro";
+                break;
+            case 10:
+                mesExtenso = "Outubro";
+                break;
+            case 11:
+                mesExtenso = "Novembro";
+                break;
+            case 12:
+                mesExtenso = "Dezembro";
+                break;
 
-    if (mes == 1) {
-        mesExtenso = "Janeiro";
-    } else if (mes == 2) {
-        mesExtenso = "Fevereiro";
-    } else if (mes == 3) {
-        mesExtenso = "Março";
-    } else if (mes == 4) {
-        mesExtenso = "Abril";
-    } else if (mes == 5) {
-        mesExtenso = "Maio";
-    } else if (mes == 6) {
-        mesExtenso = "Junho";
-    } else if (mes == 7) {
-        mesExtenso = "Julho";
-    } else if (mes == 8) {
-        mesExtenso = "Agosto";
-    } else if (mes == 9) {
-        mesExtenso = "Setembro";
-    } else if (mes == 10) {
-        mesExtenso = "Outubro";
-    } else if (mes == 11) {
-        mesExtenso = "Novembro";
-    } else if (mes == 12) {
-        mesExtenso = "Dezembro";
-    } else {
-        System.out.println("Valor inválido.");
+            default:
+                break;
+        }
+        /*
+         * modo com if
+         * if (mes == 1) {
+         * mesExtenso = "Janeiro";
+         * } else if (mes == 2) {
+         * mesExtenso = "Fevereiro";
+         * } else if (mes == 3) {
+         * mesExtenso = "Março";
+         * } else if (mes == 4) {
+         * mesExtenso = "Abril";
+         * } else if (mes == 5) {
+         * mesExtenso = "Maio";
+         * } else if (mes == 6) {
+         * mesExtenso = "Junho";
+         * } else if (mes == 7) {
+         * mesExtenso = "Julho";
+         * } else if (mes == 8) {
+         * mesExtenso = "Agosto";
+         * } else if (mes == 9) {
+         * mesExtenso = "Setembro";
+         * } else if (mes == 10) {
+         * mesExtenso = "Outubro";
+         * } else if (mes == 11) {
+         * mesExtenso = "Novembro";
+         * } else if (mes == 12) {
+         * mesExtenso = "Dezembro";
+         * } else {
+         * System.out.println("Valor inválido.");
+         * }
+         */
+        if (!mesExtenso.isEmpty()) {
+            System.out.println("O mês é: " + mesExtenso);
+        }
+
+        sc.close();
     }
-
-    if (!mesExtenso.isEmpty()) {
-        System.out.println("O mês é: " + mesExtenso);
-    }
-
-    sc.close();
 }
-}
-
--- Uni4/ex26.java
+++ Uni4/ex26.java
@@ -6,41 +6,80 @@ public class ex26 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite a opção (T, Q, R ou C):");
-        char opcao = scanner.next().charAt(0);
+        System.out.println("Digite a opção \n" +
+                "T para calcaular a area do triangulo\n" +
+                "Q para calcular a area de um quadrado\n" +
+                "R para calcular a area de um retangulo\n" +
+                "C para calcular a area de um circulo");
+        char op = scanner.next().toUpperCase().charAt(0);
 
+        double altura, base;
         double area = 0.0;
+        switch (op) {
+            case 'T':
+                System.out.println("Digite a base do triângulo:");
+                base = scanner.nextDouble();
 
-        if (opcao == 'T') {
-            System.out.println("Digite a base do triângulo:");
-            double base = scanner.nextDouble();
+                System.out.println("Digite a altura do triângulo:");
+                altura = scanner.nextDouble();
 
-            System.out.println("Digite a altura do triângulo:");
-            double altura = scanner.nextDouble();
+                area = (base * altura) / 2;
+                break;
+            case 'Q':
+                System.out.println("Digite o lado do quadrado:");
+                double lado = scanner.nextDouble();
 
-            area = (base * altura) / 2;
-        } else if (opcao == 'Q') {
-            System.out.println("Digite o lado do quadrado:");
-            double lado = scanner.nextDouble();
+                area = lado * lado;
+                break;
+            case 'R':
+                System.out.println("Digite a base do retângulo:");
+                base = scanner.nextDouble();
 
-            area = lado * lado;
-        } else if (opcao == 'R') {
-            System.out.println("Digite a base do retângulo:");
-            double base = scanner.nextDouble();
+                System.out.println("Digite a altura do retângulo:");
+                altura = scanner.nextDouble();
 
-            System.out.println("Digite a altura do retângulo:");
-            double altura = scanner.nextDouble();
+                area = base * altura;
+                break;
+            case 'C':
+                System.out.println("Digite o raio do círculo:");
+                double raio = scanner.nextDouble();
 
-            area = base * altura;
-        } else if (opcao == 'C') {
-            System.out.println("Digite o raio do círculo:");
-            double raio = scanner.nextDouble();
-
-            area = Math.PI * raio * raio;
-        } else {
-            System.out.println("Opção inválida.");
+                area = Math.PI * raio * raio;
+                break;
+            default:
+                break;
         }
-
+        /*
+         * if (op == 'T') {
+         * System.out.println("Digite a base do triângulo:");
+         * base = scanner.nextDouble();
+         * 
+         * System.out.println("Digite a altura do triângulo:");
+         * altura = scanner.nextDouble();
+         * 
+         * area = (base * altura) / 2;
+         * } else if (op == 'Q') {
+         * System.out.println("Digite o lado do quadrado:");
+         * double lado = scanner.nextDouble();
+         * 
+         * area = lado * lado;
+         * } else if (op == 'R') {
+         * System.out.println("Digite a base do retângulo:");
+         * base = scanner.nextDouble();
+         * 
+         * System.out.println("Digite a altura do retângulo:");
+         * altura = scanner.nextDouble();
+         * 
+         * area = base * altura;
+         * } else if (op == 'C') {
+         * System.out.println("Digite o raio do círculo:");
+         * double raio = scanner.nextDouble();
+         * 
+         * area = Math.PI * raio * raio;
+         * } else {
+         * System.out.println("Opção inválida.");
+         * }
+         */
         if (area != 0.0) {
             System.out.println("A área é: " + area);
         }
