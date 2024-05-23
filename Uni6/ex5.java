b377cc3ca2f79f0186adf6ed4dbb1267a1768a6f
YuriSparda <yuri.sparda@hotmail.com>
2024-05-23 19:56:48 -0300

    up

--- Uni6/ex5.java
+++ Uni6/ex5.java
@@ -1,7 +1,13 @@
 package Uni6;
 
+import java.util.Scanner;
+
 public class ex5 {
     public static void main(String[] args) {
+        int pontuação = 0;
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < args.length; i++) {
 
+        }
     }
 }
---
+++ Uni6/ex6.java
@@ -0,0 +1,34 @@
+package Uni6;
+
+import java.util.Scanner;
+
+public class ex6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("digite o numero de vetores");
+        int n = sc.nextInt();
+        int h[] = new int[n];
+        for (int i = 0; i < h.length; i++) {
+            System.out.println("digite o numero para o " + (i + 1) + "° vetor ");
+            h[i] = sc.nextInt();
+        }
+
+        System.out.println("digite o valor para pesquisar no vetor");
+        int f = sc.nextInt();
+        boolean tem = false;
+        for (int i = 0; i < h.length; i++) {
+            if (h[i] == f) {
+                tem = true;
+                System.out.println("posição " + (i + 1) + " não possui");
+            } else {
+                System.out.println("posição " + (i + 1) + " não possui");
+            }
+        }
+        if (tem == true) {
+            System.out.println("possui o numero no vetor");
+        } else {
+            System.out.println("não possui o numero no vetor");
+        }
+
+    }
+}
