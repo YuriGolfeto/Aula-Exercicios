7918a4b94963fb5bfceec96344ac3caa406c3dc2
YuriSparda <yuri.sparda@hotmail.com>
2024-06-06 20:00:01 -0300

    fix

--- Uni6/ex10.java
+++ Uni6/ex10.java
@@ -3,25 +3,25 @@ package Uni6;
 import java.util.Scanner;
 
 public class ex10 {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] array = new int[50];
+        int[] array = new int[5];
         int size = 0;
         int option;
-        
+
         do {
             System.out.println("Menu:");
-            System.out.println("1 – Add value");
-            System.out.println("2 – Search value");
-            System.out.println("3 – Update value");
-            System.out.println("4 – Delete value");
-            System.out.println("5 – Show values");
-            System.out.println("6 – Sort values");
-            System.out.println("7 – Reverse values");
-            System.out.println("8 – Exit");
+            System.out.println("1 - Add value");
+            System.out.println("2 - Search value");
+            System.out.println("3 - Update value");
+            System.out.println("4 - Delete value");
+            System.out.println("5 - Show values");
+            System.out.println("6 - Sort values");
+            System.out.println("7 - Reverse values");
+            System.out.println("8 - Exit");
             System.out.print("Choose an option: ");
             option = sc.nextInt();
-            
+
             switch (option) {
                 case 1:
                     if (size < array.length) {
@@ -38,14 +38,14 @@ public class ex10 {
                     System.out.print("Enter the value to be searched: ");
                     int searchValue = sc.nextInt();
                     boolean found = false;
-                    
+
                     for (int i = 0; i < size; i++) {
                         if (array[i] == searchValue) {
                             found = true;
                             break;
                         }
                     }
-                    
+
                     if (found) {
                         System.out.println("Value found in the array.");
                     } else {
@@ -58,7 +58,7 @@ public class ex10 {
                     System.out.print("Enter the new value: ");
                     int newValue = sc.nextInt();
                     boolean updated = false;
-                    
+
                     for (int i = 0; i < size; i++) {
                         if (array[i] == oldValue) {
                             array[i] = newValue;
@@ -66,7 +66,7 @@ public class ex10 {
                             break;
                         }
                     }
-                    
+
                     if (updated) {
                         System.out.println("Value updated in the array.");
                     } else {
@@ -77,7 +77,7 @@ public class ex10 {
                     System.out.print("Enter the value to be deleted: ");
                     int deleteValue = sc.nextInt();
                     boolean deleted = false;
-                    
+
                     for (int i = 0; i < size; i++) {
                         if (array[i] == deleteValue) {
                             deleted = true;
@@ -88,7 +88,7 @@ public class ex10 {
                             break;
                         }
                     }
-                    
+
                     if (deleted) {
                         System.out.println("Value deleted from the array.");
                     } else {
@@ -129,7 +129,7 @@ public class ex10 {
                     System.out.println("Invalid option!");
             }
         } while (option != 8);
-        
+
         sc.close();
     }
 }
